b749ba31509471885a4bb68f630ecb83f9ba3069
yayang0512 <allenreyporsuelo27@gmail.com>
2026-03-20 17:57:30 +0800

    add automated test for unified api endpoint

--- integrator/tests.py
+++ integrator/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+class IntegrationTest(TestCase):
+    def test_api_success(self):
+        response = self.client.get('/api/v1/country-weather-summary/?country=Philippines')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('current_temp_celsius', response.json())
